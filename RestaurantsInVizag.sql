9a6f9c5bab06c6d6f198ae77d8db7751cb984422
Akash132k <139198129+Akash132k@users.noreply.github.com>
2023-08-19 13:21:29 +0530

    Add files via upload

---
+++ RestaurantsInVizag.sql
@@ -0,0 +1,11 @@
+SELECT NAME
+      ,PRICE
+      ,CUSINE_CATEGORY
+      ,REGION
+      ,CUSINE_TYPE
+      ,TIMING
+      ,RATING_TYPE
+      ,RATING
+      ,VOTES
+  FROM tencombinedworkbooks
+  where RATING > 4
